54ac4a09e22f33bbabd4053512d6e10082886fd7
Kennedy <kennedygathukumi@gmail.com>
2026-07-09 10:43:01 +0300

    7

--- backend/views/departments/_form.php
+++ backend/views/departments/_form.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /** @var yii\web\View $this */
-/** @var app\models\Departments $model */
+/** @var \common\models\Departments $model */
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
